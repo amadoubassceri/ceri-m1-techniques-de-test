26c8207ac7d6bb670a8aa07da247c794777f60ea
ADMIN <narutokonoya1@gmail.com>
2024-12-15 20:04:03 +0100

    Tp6: Amelioration de PokemonFactoryTest

--- src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
@@ -1,18 +1,23 @@
 package fr.univavignon.pokedex.api;
 
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 import org.apache.commons.collections4.map.UnmodifiableMap;
 
-import fr.univavignon.pokedex.api.IPokemonFactory;
-import fr.univavignon.pokedex.api.Pokemon;
-
+/**
+ * La fabrique de Pokémon Rocket permet de créer des Pokémon
+ * avec des statistiques générées de manière aléatoire.
+ */
 public class RocketPokemonFactory implements IPokemonFactory {
 
+    /**
+     * Map associant un index à un nom de Pokémon.
+     * Ce mapping est utilisé pour donner un nom au Pokémon basé sur son index.
+     */
     private static Map<Integer, String> index2name;
+
     static {
         Map<Integer, String> aMap = new HashMap<Integer, String>();
         aMap.put(-1, "Ash's Pikachu");
@@ -24,8 +29,7 @@ public class RocketPokemonFactory implements IPokemonFactory {
 
     private static int generateRandomStat() {
         int total = 0;
-        for(int i=0; i < 1000000; i++)
-        {
+        for (int i = 0; i < 1000000; i++) {
             Random rn = new Random();
             int r = rn.nextInt(2);
             total = total + r;
@@ -36,7 +40,7 @@ public class RocketPokemonFactory implements IPokemonFactory {
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
         String name;
-        if(!index2name.containsKey(index)) {
+        if (!index2name.containsKey(index)) {
             name = index2name.get(0);
         } else {
             name = index2name.get(index);
@@ -45,7 +49,7 @@ public class RocketPokemonFactory implements IPokemonFactory {
         int defense;
         int stamina;
         double iv;
-        if(index < 0) {
+        if (index < 0) {
             attack = 1000;
             defense = 1000;
             stamina = 1000;
@@ -58,5 +62,4 @@ public class RocketPokemonFactory implements IPokemonFactory {
         }
         return new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
     }
-
 }
