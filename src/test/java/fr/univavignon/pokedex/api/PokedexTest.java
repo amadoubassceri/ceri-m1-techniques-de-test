d0c5eed9318ef36424d27eabf11b2739b30e0b68
ADMIN <narutokonoya1@gmail.com>
2024-12-15 19:12:18 +0100

    Tp4: Amelioration de PokedexTest

--- src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
@@ -87,6 +87,36 @@ public class PokedexTest {
     }
 
     @Test
+    public void testGetPokemonsWithReverseSorting() {
+        Pokemon bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+        Pokemon aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100.0);
+
+        pokedex.addPokemon(bulbizarre);
+        pokedex.addPokemon(aquali);
+
+        // Tri basé sur le CP, dans l'ordre inverse
+        List<Pokemon> sortedPokemons = pokedex.getPokemons(Comparator.comparingInt(Pokemon::getCp).reversed());
+
+        // Vérifier que la liste est triée dans l'ordre inverse par CP
+        assertEquals("Aquali", sortedPokemons.get(0).getName());
+        assertEquals("Bulbizarre", sortedPokemons.get(1).getName());
+    }
+
+    @Test
+    public void testAddMultiplePokemons() {
+        Pokemon bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+        Pokemon aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100.0);
+
+        // Ajouter plusieurs Pokémon
+        int index1 = pokedex.addPokemon(bulbizarre);
+        int index2 = pokedex.addPokemon(aquali);
+
+        // Vérifier que les index sont corrects
+        assertEquals(0, index1);
+        assertEquals(1, index2);
+    }
+
+    @Test
     public void testGetPokemonMetadata() throws PokedexException {
         PokemonMetadata metadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
         when(metadataProvider.getPokemonMetadata(0)).thenReturn(metadata);
@@ -95,5 +125,14 @@ public class PokedexTest {
         PokemonMetadata returnedMetadata = pokedex.getPokemonMetadata(0);
         assertNotNull(returnedMetadata);
         assertEquals("Bulbizarre", returnedMetadata.getName());
+    }
+
+    @Test(expected = PokedexException.class)
+    public void testGetPokemonMetadataThrowsException() throws PokedexException {
+        // Simuler une exception lors de la récupération des métadonnées
+        when(metadataProvider.getPokemonMetadata(0)).thenThrow(new PokedexException("Test Exception"));
+
+        // Appeler getPokemonMetadata, ce qui doit lancer l'exception
+        pokedex.getPokemonMetadata(0);
     }
 }
